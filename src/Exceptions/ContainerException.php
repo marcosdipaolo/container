5b0ddc87acac296d50559531d6e8e84e139ee601
Marcos Di Paolo <marcosdipaolo@gmail.com>
2026-04-04 21:22:10 -0300

    feat: claude hiaku 4.5 iprovements

--- src/Exceptions/ContainerException.php
+++ src/Exceptions/ContainerException.php
@@ -1,9 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MDP\Container\Exceptions;
 
 use Psr\Container\ContainerExceptionInterface;
 
+/**
+ * Exception thrown when a container error occurs.
+ *
+ * This exception is thrown when the container encounters an error during
+ * service resolution or binding, such as uninstantiable classes, missing
+ * type hints, or invalid configurations.
+ */
 class ContainerException extends \Exception implements ContainerExceptionInterface
 {
 }
